2d7ea3a9ce3f1e1897322f70082419a0309f48d3
gabriiellmoura <josegabriiell81@gmail.com>
2025-07-15 17:40:37 -0300

    adiciona arquivos python da pasta progra_orientada_objetos

---
+++ python_codigos_organizado/codigos-python/orientacao-objetos/aula22.py
@@ -0,0 +1,14 @@
+class A:
+    def __new__(cls, *args, **kwargs):
+        instancia = super().__new__(cls)
+        return instancia
+
+    def __init__(self, x):
+        self.x = x
+        
+
+    def __repr__(self):
+        return 'A()'
+
+
+a = A(123)
--- python_codigos_organizado/codigos-python/orientacao-objetos/classe1.py
+++
@@ -1,4 +0,0 @@
-# Exemplo de classe Python
-class MinhaClasse:
-    def __init__(self):
-        pass
