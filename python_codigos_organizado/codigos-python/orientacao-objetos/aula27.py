c1176a8e4f8a993b6068f4119fb30607edb80819
gabriiellmoura <josegabriiell81@gmail.com>
2025-08-04 12:54:20 -0300

    adiciona arquivos python a pasta progra_orientada_objetos

---
+++ python_codigos_organizado/codigos-python/orientacao-objetos/aula27.py
@@ -0,0 +1,16 @@
+class Multiplicar:
+    def __init__(self, multiplicador):
+        self._multiplicador = multiplicador
+
+    def __call__(self, func):
+        def interna(*args, **kwargs):
+            resultado = func(*args, **kwargs)
+            return resultado * self._multiplicador
+        return interna
+
+@Multiplicar(5)
+def soma(x, y):
+    return x + y
+
+operacao = soma(2, 4)
+print(operacao)
